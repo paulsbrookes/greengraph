3f152109787074a1d3fabe9e835eac280f06a746
paulsbrookes <paul.s.brookes@gmail.com>
2015-12-22 20:54:21 +0000

    Fixed setup.py by including a packages line.

--- setup.py
+++ setup.py
@@ -3,5 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name = "greengraph",
     version = "0.1",
+    packages = find_packages(exclude=['*test']),
     install_requires = ['geopy','numpy','matplotlib','requests']
 )
